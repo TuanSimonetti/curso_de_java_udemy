b2318d2deaa0d16a898a5fad398715b17639808f
TuanSimonetti <54821029+TuanSimonetti@users.noreply.github.com>
2020-07-24 21:43:43 -0300

    Objeto Vs Primitivos

---
+++ src/fundamentos/objetoVsPrimitivo/objetoVsPrimitivo.java
@@ -0,0 +1,18 @@
+package fundamentos.objetoVsPrimitivo;
+
+public class objetoVsPrimitivo {
+    public static void maind(String[] args) {
+
+        String s = "texto";
+        s.toUpperCase();
+
+        String r = new String("Texto2");
+        r.toLowerCase();
+
+
+        // Wrappers (Aquilo que envolve)
+        // Wrappers é a versão do objeto dos tipos primitivos
+        int a = 123;
+
+    }
+}
